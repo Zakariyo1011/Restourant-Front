d91b4326fead5b015077ce7118d14991afd99d06
Zakariyo1011 <zhacker1011z@gmail.com>
2026-05-15 10:09:39 +0500

    Fix image URLs for ImageKit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -228,7 +228,7 @@
             <div class="card-img-wrap">
                 <img
                     v-if="r.image_path"
-                    :src="`https://restourant-production-6ae5.up.railway.app/storage/${r.image_path}`"
+                    :src="r.image_path"
                     :alt="r.name"
                     class="card-img"
                 />
